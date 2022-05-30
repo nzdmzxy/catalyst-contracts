8fd9522a5ffe4c5dc13849084d988a5512c5bfbc
menduz <github@menduz.com>
2022-05-30 12:43:18 -0300

    faster tests

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,6 @@
 import RequestManager, { ContractFactory } from 'eth-connect'
-import {
-  CatalystContract,
-  catalystDeployments,
-  catalystAbiItems
-} from './CatalystAbi'
-import {
-  listAbiItems,
-  denylistNamesDeployments,
-  ListContract,
-  poiDeployments
-} from './ListAbi'
+import { CatalystContract, catalystDeployments, catalystAbiItems, CatalystByIdResult } from './CatalystAbi'
+import { listAbiItems, denylistNamesDeployments, ListContract, poiDeployments } from './ListAbi'
 
 // eslint-disable-next-line
 export async function nameDenylistForProvider(ethereumProvider: any): Promise<ListContract> {
@@ -17,15 +8,11 @@ export async function nameDenylistForProvider(ethereumProvider: any): Promise<Li
   const networkId = (await rm.net_version()).toString()
 
   if (!(networkId in denylistNamesDeployments))
-    throw new Error(
-      `There is no deployed NameDenylist contract for networkId=${networkId}`
-    )
+    throw new Error(`There is no deployed NameDenylist contract for networkId=${networkId}`)
 
   const contractAddress = denylistNamesDeployments[networkId]
 
-  return (await new ContractFactory(rm, listAbiItems).at(
-    contractAddress
-  )) as any as ListContract
+  return (await new ContractFactory(rm, listAbiItems).at(contractAddress)) as any as ListContract
 }
 
 // eslint-disable-next-line
@@ -34,15 +21,11 @@ export async function poiListForProvider(ethereumProvider: any): Promise<ListCon
   const networkId = (await rm.net_version()).toString()
 
   if (!(networkId in poiDeployments))
-    throw new Error(
-      `There is no deployed PoiDenylist contract for networkId=${networkId}`
-    )
+    throw new Error(`There is no deployed PoiDenylist contract for networkId=${networkId}`)
 
   const contractAddress = poiDeployments[networkId]
 
-  return (await new ContractFactory(rm, listAbiItems).at(
-    contractAddress
-  )) as any as ListContract
+  return (await new ContractFactory(rm, listAbiItems).at(contractAddress)) as any as ListContract
 }
 
 // eslint-disable-next-line
@@ -51,13 +34,45 @@ export async function catalystRegistryForProvider(ethereumProvider: any): Promis
   const networkId = (await rm.net_version()).toString()
 
   if (!(networkId in catalystDeployments))
-    throw new Error(
-      `There is no deployed CatalystProxy contract for networkId=${networkId}`
-    )
+    throw new Error(`There is no deployed CatalystProxy contract for networkId=${networkId}`)
 
   const contractAddress = catalystDeployments[networkId]
 
-  return (await new ContractFactory(rm, catalystAbiItems).at(
-    contractAddress
-  )) as any as CatalystContract
+  return (await new ContractFactory(rm, catalystAbiItems).at(contractAddress)) as any as CatalystContract
+}
+
+/** Returns the catalyst list for a specified Ethereum Provider. */
+export async function getAllCatalystFromProvider(
+  // eslint-disable-next-line
+  etherumProvider: any
+): Promise<CatalystByIdResult[]> {
+  const contract = await catalystRegistryForProvider(etherumProvider)
+
+  const count = (await contract.catalystCount()).toNumber()
+
+  const nodes: CatalystByIdResult[] = []
+  // Create an array with values from 0 to count - 1
+  const indices = new Array(count).fill(0).map((_, i) => i)
+
+  const dataPromises = indices.map((index) => contract.catalystIds(index).then((id) => contract.catalystById(id)))
+
+  const data = await Promise.all(dataPromises)
+
+  for (const node of data) {
+    if (node.domain.startsWith('http://')) {
+      console.warn(`Catalyst node domain using http protocol, skipping ${JSON.stringify(node)}`)
+      continue
+    }
+
+    if (!node.domain.startsWith('https://')) {
+      node.domain = 'https://' + node.domain
+    }
+
+    // trim url in case it starts/ends with a blank
+    node.domain = node.domain.trim()
+
+    nodes.push(node)
+  }
+
+  return nodes
 }
